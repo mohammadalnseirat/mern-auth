16779854b9e4b759abe180a6df9ee817bed231ad
mohammadalnseirat <146799067+mohammadalnseirat@users.noreply.github.com>
2024-07-25 03:51:45 +0400

    create a user model

---
+++ api/models/user.model.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+// create a new Schema
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+// create and export the model:
+const User = mongoose.model("User", userSchema);
+export default User;
