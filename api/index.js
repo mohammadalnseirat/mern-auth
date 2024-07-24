164befa37d62422ecdba6892e4591eda643a1e00
mohammadalnseirat <146799067+mohammadalnseirat@users.noreply.github.com>
2024-07-24 06:51:11 +0400

    connected to database

--- api/index.js
+++ api/index.js
@@ -1,10 +1,20 @@
-import express from 'express';
-const port = process.env.PORT || 3000
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+const port = process.env.PORT || 3000;
+dotenv.config();
+
 const app = express();
 
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
-
-
-app.listen(port,()=>{
-    console.log(`Server is running on port ${port}`)  // server is running on port 3000
-})
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`); // server is running on port 3000
+});
