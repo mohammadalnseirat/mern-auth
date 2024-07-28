355c458c99ba149f8091886fcc650cf0e976c7ff
mohammadalnseirat <146799067+mohammadalnseirat@users.noreply.github.com>
2024-07-28 20:22:39 +0400

    complete sign in page ui functionality

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,61 +1,103 @@
-import React, { useState } from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import { FaEye, FaEyeSlash } from "react-icons/fa6";
 
 const SignIn = () => {
-  const [showPassword,setShowPassword] =useState(false)
-  const [loading, setLoading] = useState(false)
+  const [showPassword, setShowPassword] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+  const [formData, setFormData] = useState({});
+  const navigate = useNavigate();
+
+  // handle change data from the input field:
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value });
+  };
+
+  // handle submit and fetch data from the api:
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      setLoading(true);
+      setError(false);
+      // create a response:
+      const res = await fetch("/api/auth/signin", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(formData),
+      });
+
+      // convert response to json:
+      const data = await res.json();
+      // if success:
+      if (data.success === false) {
+        setError(true);
+        setLoading(false);
+        return;
+      }
+      setLoading(false);
+      setError(false);
+      // navigate to home page:
+      navigate("/");
+    } catch (error) {
+      setLoading(false);
+      setError(true);
+    }
+  };
   return (
     <div className="p-3 max-w-lg mx-auto">
-    <h1 className="text-3xl font-semibold my-7 text-center">Sign In</h1>
-    {/* Sign Up Form Start Here */}
-    <form  className="flex flex-col gap-5">
-      <input
-        type="email"
-        id="email"
-        placeholder="Email"
-        className="bg-slate-100 p-3 rounded-lg border border-blue-300 focus:outline-none"
-       
-      />
-      <div className="relative">
+      <h1 className="text-3xl font-semibold my-7 text-center">Sign In</h1>
+      {/* Sign Up Form Start Here */}
+      <form onSubmit={handleSubmit} className="flex flex-col gap-5">
         <input
-          type={showPassword ? "text" : "password"}
-          id="password"
-          placeholder="Password"
-          className="bg-slate-100 p-3 w-full rounded-lg border border-blue-300 focus:outline-none"
-          
+          type="email"
+          id="email"
+          placeholder="Email"
+          className="bg-slate-100 p-3 rounded-lg border border-blue-300 focus:outline-none"
+          onChange={handleChange}
         />
-        {showPassword ? (
-          <FaEyeSlash
-            className="absolute right-4 top-4 text-xl cursor-pointer"
-            onClick={() => setShowPassword(!showPassword)}
+        <div className="relative">
+          <input
+            type={showPassword ? "text" : "password"}
+            id="password"
+            placeholder="Password"
+            className="bg-slate-100 p-3 w-full rounded-lg border border-blue-300 focus:outline-none"
+            onChange={handleChange}
           />
-        ) : (
-          <FaEye
-            className="absolute right-4 top-4 text-xl cursor-pointer"
-            onClick={() => setShowPassword(!showPassword)}
-          />
-        )}
+          {showPassword ? (
+            <FaEyeSlash
+              className="absolute right-4 top-4 text-xl cursor-pointer"
+              onClick={() => setShowPassword(!showPassword)}
+            />
+          ) : (
+            <FaEye
+              className="absolute right-4 top-4 text-xl cursor-pointer"
+              onClick={() => setShowPassword(!showPassword)}
+            />
+          )}
+        </div>
+        <button
+          disabled={loading}
+          className="bg-slate-700 p-3 uppercase text-white rounded-lg hover:rounded-full  hover:bg-opacity-90 disabled:opacity-80 disabled:rounded-full transition-all duration-150"
+        >
+          {loading ? "Loading..." : "Sign in"}
+        </button>
+      </form>
+      {/* Sign Up Form End Here */}
+      <div className="flex items-center gap-2 mt-5">
+        <p className="text-gray-500"> Dont Have an Account?</p>
+        <Link
+          to={"/sign-up"}
+          className="text-blue-800 underline font-[500] hover:text-red-700 transition-all duration-150"
+        >
+          Sign Up
+        </Link>
       </div>
-      <button
-        disabled={loading}
-        className="bg-slate-700 p-3 uppercase text-white rounded-lg hover:rounded-full  hover:bg-opacity-90 disabled:opacity-80 disabled:rounded-full transition-all duration-150"
-      >
-        {loading ? "Loading..." : "Sign in"}
-      </button>
-    </form>
-    {/* Sign Up Form End Here */}
-    <div className="flex items-center gap-2 mt-5">
-      <p className="text-gray-500"> Dont Have an Account?</p>
-      <Link
-        to={"/sign-up"}
-        className="text-blue-800 underline font-[500] hover:text-red-700 transition-all duration-150"
-      >
-        Sign Up
-      </Link>
+      {error && (
+        <p className="text-red-600 font-semibold mt-5">Failed to Sign In...</p>
+      )}
     </div>
-    </div>
-  )
-}
+  );
+};
 
-export default SignIn
+export default SignIn;
